d315b489873087b68a44b3db98b7ec96ead77bd3
Simon Lee <simonlee@me.com>
2023-05-20 11:26:27 +0800

    Updated: 2023-05-20 11:26 AM

--- SS.py
+++ SS.py
@@ -73,7 +73,7 @@ st.set_page_config(page_title='SS')
 ######
 with st.form('form'):
   url = st.text_input('URL',key='url')
-  col1, col2 = st.columns(3)
+  col1, col2 = st.columns(2)
   with col1:
     isSubmit = st.form_submit_button('Submit')
   with col2:
